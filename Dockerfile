df7168f99ec4d20865af2a0ce130f30bea146d46
maxgio92 <massimiliano.giovagnoli.1992@gmail.com>
2021-01-19 14:44:04 +0100

    fix(entrypoint.sh): configure helper for specified url context

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,9 @@
 ARG TERRAFORM_VERSION=0.13.6
 FROM hashicorp/terraform:$TERRAFORM_VERSION
 
-ENV GIT_USER ''
+ENV GIT_AUTH_URL ''
 ENV GIT_TOKEN ''
+ENV GIT_USER ''
 ENV GIT_PASS ''
 
 COPY entrypoint.sh /entrypoint.sh
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-set -e
+set -eu
 
 if [ ! -z "${GIT_TOKEN}" -a ! -z "${GIT_AUTH_URL}" ]; then
 	git config --global http.${GIT_AUTH_URL}.extraheader "AUTHORIZATION: bearer ${GIT_TOKEN}"
-elif [ ! -z "${GIT_PASS}" -a ! -z "${GIT_PASS}"]; then
-	git config --global credential.helper '!f() { sleep 1; echo "username=${GIT_USER}"; echo "password=${GIT_PASS}"; }; f'
+elif [ ! -z "${GIT_USER}" -a ! -z "${GIT_PASS}" -a ! -z "${GIT_AUTH_URL}" ]; then
+	git config --global credential.${GIT_AUTH_URL}.helper '!f() { sleep 1; echo "username=${GIT_USER}"; echo "password=${GIT_PASS}"; }; f'
 fi
 
 exec $@
