0ae3f37f6fd7c71a163de6ef08c711d909916a91
maxgio92 <massimiliano.giovagnoli.1992@gmail.com>
2021-01-19 12:25:02 +0100

    feat(entrypoint.sh): enable to configure bearer token auth for specific urls

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,11 +2,10 @@
 
 set -e
 
-if [ ! -z "${GIT_TOKEN}" ]; then
-	git config --global http.extraheader "AUTHORIZATION: bearer ${GIT_TOKEN}"
+if [ ! -z "${GIT_TOKEN}" -a ! -z "${GIT_AUTH_URL}" ]; then
+	git config --global http.${GIT_AUTH_URL}.extraheader "AUTHORIZATION: bearer ${GIT_TOKEN}"
 elif [ ! -z "${GIT_PASS}" -a ! -z "${GIT_PASS}"]; then
 	git config --global credential.helper '!f() { sleep 1; echo "username=${GIT_USER}"; echo "password=${GIT_PASS}"; }; f'
 fi
 
-#terraform $@
 exec $@
